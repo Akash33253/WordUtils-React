d9d6222758161f1917316f6089386ab27557d97b
Akash33253 <akashgtcatopk@gmail.com>
2022-11-08 20:15:07 +0530

    logo introduced in navbar

---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo1 from "./logo1.png"
+export default function Logo() {
+  return (
+    <div className='mx-3'>
+      <img src={logo1} id='logo1' alt=""/>
+    </div>
+  )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Logo from './Logo';
 // import { a } from 'react-router-dom';
 
 export default function Navbar(props) {
@@ -31,6 +32,7 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
+    <Logo/>
     <a className="navbar-brand" href="#">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
